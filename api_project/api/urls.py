9a13fd0d59a5d36ae432d302ac632ca23b27bdd9
ahmat Mahamat <captainahmat91@gmail.com>
2025-11-18 14:08:55 +0200

    new commits

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    
+     # Token authentication endpoint
+    path('auth-token/', obtain_auth_token, name='api_token_auth'),
 
     # Router-generated CRUD URLs
     path('', include(router.urls)),
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     # Third-party
     'rest_framework',
+    'rest_framework.authtoken',
     # Local apps
     'api',
 ]
@@ -125,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
